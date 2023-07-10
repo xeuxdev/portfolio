d5ccd69233d1f91624877de631ccdaf9ca8e39b0
Headbwoi <headbwoi25@gmail.com>
2023-07-10 12:49:01 +0100

    update about page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
